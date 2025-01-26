368c697135b0b50fe4e171f5892652cc482f74fb
Andrey Kataev <67541326+paych3ck@users.noreply.github.com>
2025-01-26 22:57:15 +0300

    Работа над авто-деплоем

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import requests
+import git
 from flask import Flask, request
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+REPO_PATH = os.getenv("GITHUB_REPO_PATH")
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 
-def send_message(text):
+def send_info(text):
     data = {"chat_id": CHAT_ID, "text": text}
     requests.post(URL, data=data)
 
@@ -15,8 +17,19 @@ def send_message(text):
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST"])
-def webhook():
+@app.route("/update_repo", methods=["POST"])
+def update_repo():
+    try:
+        repo = git.Repo(REPO_PATH)
+        origin = repo.remotes.origin
+        origin.pull()
+        return 200
+    except Exception:
+        return 500
+
+
+@app.route("/send_message", methods=["POST"])
+def send_message():
     data = request.json
     if not data:
         return 400
@@ -28,7 +41,7 @@ def webhook():
             [f"- {commit['message']}" for commit in data["commits"]])
         text = f"🚀 Новый коммит в репозитории *{repo}* от {pusher}:\n{commit_messages}"
 
-        send_message(text)
+        send_info(text)
         return 200
 
     return 200
