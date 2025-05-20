d105f2e53527e9043c2506da5f40a02e984d99b3
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-05-20 16:22:43 +0300

    DEB-6: тесты для хендлеров (#8)

---
+++ tests/handler/test_commit_event.py
@@ -0,0 +1,39 @@
+import pytest
+from flask import Flask
+import app.handler.commit_event as commit_handler
+
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    app.register_blueprint(commit_handler.commit_bp)
+    return app.test_client()
+
+
+def test_commit_event_no_data(client):
+    response = client.post(
+        "/webhook/commits",
+        json={},
+    )
+
+    assert response.status_code == 400
+    assert response.get_json() == {"error": "No data received"}
+
+
+def test_commit_event_with_data(mocker, client):
+    data = {"key": "value"}
+    mock_process_commit_event = mocker.patch.object(
+        commit_handler,
+        "process_commit_event",
+        return_value=("Commit event processed", 200),
+    )
+
+    response = client.post(
+        "/webhook/commits",
+        json=data,
+    )
+
+    mock_process_commit_event.assert_called_once_with(data)
+
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Commit event processed"}
---
+++ tests/handler/test_github_actions_failed_event.py
@@ -0,0 +1,39 @@
+import pytest
+from flask import Flask
+import app.handler.github_actions_failed_event as github_actions_failed_handler
+
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    app.register_blueprint(github_actions_failed_handler.actions_bp)
+    return app.test_client()
+
+
+def test_github_actions_failed_event_no_data(client):
+    response = client.post(
+        "/webhook/github_actions",
+        json={},
+    )
+
+    assert response.status_code == 400
+    assert response.get_json() == {"error": "No data received"}
+
+
+def test_github_actions_failed_event_with_data(mocker, client):
+    data = {"key": "value"}
+    mock_process_github_actions_failed_event = mocker.patch.object(
+        github_actions_failed_handler,
+        "process_github_actions_failed_event",
+        return_value=("Workflow completed successfully", 200),
+    )
+
+    response = client.post(
+        "/webhook/github_actions",
+        json=data,
+    )
+
+    mock_process_github_actions_failed_event.assert_called_once_with(data)
+
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Workflow completed successfully"}
