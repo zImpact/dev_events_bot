48ff2ea2680f4ba8f985d8e423e9986941a93160
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-05-20 16:16:24 +0300

    DEB-9: форматирование нижнего подчеркивания (#9)

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,10 @@
+import re
 from app.config import TG_IDS
 
 
 def github_to_tg(nickname):
     return TG_IDS.get(nickname, -1)
+
+def escape_markdown(text):
+    escape_chars = r"_*[]()~`>#+-=|{}.!"
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
