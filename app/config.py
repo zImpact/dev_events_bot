49090dc95f31725649fde86efea25c5828cb3342
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-05-20 16:19:00 +0300

    DEB-2: назначение ревьювера (#6)

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 import os
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-REPO_PATH = os.getenv("GITHUB_REPO_PATH")
-WSGI_PATH = os.getenv("SERVER_WSGI_PATH")
-THREAD_ID = os.getenv("TELEGRAM_THREAD_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+GITHUB_REPO_PATH = os.getenv("GITHUB_REPO_PATH")
+SERVER_WSGI_PATH = os.getenv("SERVER_WSGI_PATH")
+TELEGRAM_THREAD_ID = os.getenv("TELEGRAM_THREAD_ID")
+JIRA_BASE_URL = os.getenv("JIRA_BASE_URL")
+JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
+JIRA_LOGIN_EMAIL = os.getenv("JIRA_LOGIN_EMAIL")
 
 REPO_NAMES = {
     "yn": "Яна",
@@ -16,8 +19,24 @@ REPO_NAMES = {
 }
 
 TG_IDS = {
-    "DmytroMamedbekov": os.getenv("DMYTRO_ID"),
-    "paych3ck": os.getenv("ANDREY_ID"),
-    "Buhicevskij": os.getenv("DANYA_ID"),
-    "Aleron-Meredi": os.getenv("EGOR_ID")
+    "DmytroMamedbekov": os.getenv("DMYTRO_TG_ID"),
+    "paych3ck": os.getenv("ANDREY_TG_ID"),
+    "Buhicevskij": os.getenv("DANYA_TG_ID"),
+    "Aleron-Meredi": os.getenv("EGOR_TG_ID")
+}
+
+JIRA_IDS = {
+    "DmytroMamedbekov": os.getenv("DMYTRO_JIRA_ID"),
+    "paych3ck": os.getenv("ANDREY_JIRA_ID"),
+    "Buhicevskij": os.getenv("DANYA_JIRA_ID")
+}
+
+JIRA_REVIEWER_FIELD_IDS = {
+    "yn": 10092,
+    "din": 10042,
+    "tmlp": 10041,
+    "osd": 10091,
+    "thld": 10039,
+    "bsar": 10093,
+    "dev_events_bot": 10040
 }
--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,12 @@
-import re
-from app.config import TG_IDS
-
+from app.config import TG_IDS, JIRA_IDS
 
 def github_to_tg(nickname):
     return TG_IDS.get(nickname, -1)
 
+def github_to_jira(nickname):
+    return JIRA_IDS.get(nickname, -1)
+
 def escape_markdown(text):
     escape_chars = r"_*[]()~`>#+-=|{}.!"
     return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
+
